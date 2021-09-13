371044a81f81412486fb69b5b11299108b23001a
SEONG NAMKUNG <castello@naver.com>
2021-09-14 01:20:59 +0900

    Create RequestInfo.java

---
+++ ch2/RequestInfo.java
@@ -0,0 +1,62 @@
+package com.fastcampus.ch2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Calendar;
+
+@Controller
+public class RequestInfo {
+    @RequestMapping("/requestInfo")
+    //    public static void main(String[] args) {
+    public void main(HttpServletRequest request) {
+        System.out.println("request.getCharacterEncoding()="+request.getCharacterEncoding()); // 요청 내용의 인코딩
+        System.out.println("request.getContentLength()="+request.getContentLength());  // 요청 내용의 길이. 알수 없을 때는 -1
+        System.out.println("request.getContentType()="+request.getContentType()); // 요청 내용의 타입. 알 수 없을 때는 null
+
+        System.out.println("request.getMethod()="+request.getMethod());      // 요청 방법
+        System.out.println("request.getProtocol()="+request.getProtocol());  // 프로토콜의 종류와 버젼 HTTP/1.1
+        System.out.println("request.getScheme()="+request.getScheme());      // 프로토콜
+
+        System.out.println("request.getServerName()="+request.getServerName()); // 서버 이름 또는 ip주소
+        System.out.println("request.getServerPort()="+request.getServerPort()); // 서버 포트
+        System.out.println("request.getRequestURI()="+request.getRequestURI()); // 요청 URI
+
+        System.out.println("request.getContextPath()="+request.getContextPath()); // context path
+        System.out.println("request.getServletPath()="+request.getServletPath()); // servlet path
+        System.out.println("request.getQueryString()="+request.getQueryString()); // 쿼리 스트링
+
+        System.out.println("request.getLocalName()="+request.getLocalName()); // 로컬 이름
+        System.out.println("request.getLocalPort()="+request.getLocalPort()); // 로컬 포트
+
+        System.out.println("request.getRemoteAddr()="+request.getRemoteAddr()); // 원격 ip주소
+        System.out.println("request.getRemoteHost()="+request.getRemoteHost()); // 원격 호스트 또는 ip주소
+        System.out.println("request.getRemotePort()="+request.getRemotePort()); // 원격 포트
+    }
+}
+
+// AWS에 배포(deploy)한 다음에 실행하면, 실제 ip주소를 확인할 수 있음.
+/* http://localhost:8080/ch2/requestInfo?year=2021&month=10&day=1
+request.getCharacterEncoding()=UTF-8
+request.getContentLength()=-1
+request.getContentType()=null
+request.getMethod()=GET
+request.getProtocol()=HTTP/1.1
+request.getScheme()=http
+request.getServerName()=localhost
+request.getServerPort()=8080
+request.getRequestURI()=/ch2/requestInfo
+request.getContextPath()=/ch2
+request.getServletPath()=/requestInfo
+request.getQueryString()=year=2021&month=10&day=1
+request.getLocalName()=localhost
+request.getLocalPort()=8080
+request.getRemoteAddr()=0:0:0:0:0:0:0:1
+request.getRemoteHost()=0:0:0:0:0:0:0:1
+request.getRemotePort()=54855
+
+*/
