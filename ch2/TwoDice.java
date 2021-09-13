2dcc3e381c3ef7b409be426d0b0c5e67b4fbe007
SEONG NAMKUNG <castello@naver.com>
2021-09-14 01:56:57 +0900

    Create TwoDice.java

---
+++ ch2/TwoDice.java
@@ -0,0 +1,31 @@
+package com.fastcampus.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Controller
+public class TwoDice {
+    @RequestMapping("/roll")
+    //    public static void main(String[] args) {
+    public void main(HttpServletResponse response) throws IOException {
+        int idx1 = (int)(Math.random()*6)+1;
+        int idx2 = (int)(Math.random()*6)+1;
+
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<img src='resources/img/dice"+idx1+".jpg'>");
+        out.println("<img src='resources/img/dice"+idx2+".jpg'>");
+        out.println("</body>");
+        out.println("</html>");
+        out.close();
+    }
+}
