f7324689538c333241d9cf8dd9ae7d2c56c68d4a
SEONG NAMKUNG <castello@naver.com>
2021-09-13 23:52:58 +0900

    Create PrivateMethodCall.java

---
+++ ch2/PrivateMethodCall.java
@@ -0,0 +1,21 @@
+package com.fastcampus.ch2;
+
+import java.lang.reflect.Method;
+
+public class PrivateMethodCall {
+	public static void main(String[] args) throws Exception{
+//		Hello hello = new Hello();
+//		hello.main(); // private이라서 외부 호출 불가
+		
+		// Reflection API를 사용 - 클래스 정보를 얻고 다룰 수 있는 강력한 기능제공
+		// java.lang.reflect패키지를 제공
+		// Hello클래스의 Class객체(클래스의 정보를 담고 있는 객체)를 얻어온다.
+		Class helloClass = Class.forName("com.fastcampus.ch2.Hello");
+		Hello hello = (Hello)helloClass.newInstance(); // Class객체가 가진 정보로 객체 생성 
+		Method main = helloClass.getDeclaredMethod("main");
+		main.setAccessible(true); // priavte인 main()을 호출가능하게 한다.
+		
+		main.invoke(hello); // hello.main()
+		
+	}
+}
