83a5e7f6cb372ad790c8b7757dfaa56efb24bfd9
SEONG NAMKUNG <castello@naver.com>
2021-09-15 01:07:58 +0900

    Update RequestInfo.java

--- ch2/RequestInfo.java
+++ ch2/RequestInfo.java
@@ -4,10 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Calendar;
 
 @Controller
 public class RequestInfo {
