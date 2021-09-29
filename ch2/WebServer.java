9fd054013d8688d19f9ca573225e5aa3960fcd92
SEONG NAMKUNG <castello@naver.com>
2021-09-29 16:27:05 +0900

    Update WebServer.java

--- ch2/WebServer.java
+++ ch2/WebServer.java
@@ -4,21 +4,25 @@ import java.net.Socket;
 
 public class WebServer {
         public static void main(String[] args) throws Exception {
-                ServerSocket serverSocket = new ServerSocket(8888);
+            ServerSocket serverSocket = new ServerSocket(8888);
 
-                System.out.println("서버가 요청을 받을 준비가 되었습니다.");
-                System.out.println("연결을 기다리는 중...");
-                Socket socket = serverSocket.accept();
-                System.out.println("연결되었습니다.");
+            System.out.println("서버가 요청을 받을 준비가 되었습니다.");
+            System.out.println("연결을 기다리는 중...");
+            Socket socket = serverSocket.accept();
+            System.out.println("연결되었습니다.");
+            InputStream in = socket.getInputStream();
 
-                InputStream in = socket.getInputStream();
-                BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            byte[] byteArr = new byte[8192];
 
-                int data = 0;
-                while ((data = br.read()) != -1)
-                        System.out.print((char) data);
+            in.read(byteArr);
+            String request = new String(byteArr, "utf-8"); // byte -> String
+                
+            String[] lines = request.split("\n");
+            System.out.println("request="+request);
+            System.out.println("line cnt="+lines.length);
 
-                br.close();
-                socket.close();
+            System.out.println(lines[0]); // GET /ch2/getYoil HTTP/1.1
+            System.out.println(lines[0].split(" ")[1]);// /ch2/getYoil
+
         }
-}
+    }
