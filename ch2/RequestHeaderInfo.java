84db797e37679673e6956fec9cb2e5f5db580b3c
SEONG NAMKUNG <castello@naver.com>
2021-09-30 09:17:51 +0900

    Update RequestHeaderInfo.java

--- ch2/RequestHeaderInfo.java
+++ ch2/RequestHeaderInfo.java
@@ -1,9 +1,7 @@
 package com.fastcampus.ch2;
 
 import java.util.Enumeration;
-
 import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
