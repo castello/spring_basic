37d5b771e93bddfaab8f65a6f40a2ce8d1bde713
SEONG NAMKUNG <castello@naver.com>
2021-09-30 10:48:09 +0900

    Create RequestHeaderInfo.java

---
+++ ch2/RequestHeaderInfo.java
@@ -0,0 +1,38 @@
+package com.fastcampus.ch2;
+
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RequestHeaderInfo {
+	@RequestMapping("/requestHeaderInfo")
+	public void main(HttpServletRequest request) {
+		
+		Enumeration<String> e = request.getHeaderNames();
+
+		while (e.hasMoreElements()) {
+			String name = e.nextElement();
+			System.out.println(name + ":" + request.getHeader(name));
+		}
+	}
+}
+
+[실행결과]
+host:localhost:8080
+connection:keep-alive
+sec-ch-ua:"Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"
+sec-ch-ua-mobile:?0
+sec-ch-ua-platform:"macOS"
+upgrade-insecure-requests:1
+user-agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36
+accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
+sec-fetch-site:none
+sec-fetch-mode:navigate
+sec-fetch-user:?1
+sec-fetch-dest:document
+accept-encoding:gzip, deflate, br
+accept-language:ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7
