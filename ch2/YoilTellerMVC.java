95ff66b68dbdefae072ca6d74c5297f5ed46939c
SEONG NAMKUNG <castello@naver.com>
2021-10-02 23:45:43 +0900

    Update YoilTellerMVC.java

--- ch2/YoilTellerMVC.java
+++ ch2/YoilTellerMVC.java
@@ -9,29 +9,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class YoilTellerMVC {
-    @RequestMapping("/getYoilMVC") // http://localhost/ch2/getYoilMVC?year=2021&month=10&day=1
-    public String main(int year, int month, int day, Model model) throws IOException {
+public class YoilTellerMVC2 {
+    @RequestMapping("/getYoilMVC2") // http://localhost/ch2/getYoilMVC2
+    public String main(
+    		@RequestParam(defaultValue="-1") int year, 
+    		@RequestParam(defaultValue="-1") int month, 
+    		@RequestParam(defaultValue="-1") int day, 
+    		Model model
+    ) throws IOException {
 
+        // 1. 유효성 검사
     	if(!isValid(year, month, day)) 
-    		return "yoilError";  // /WEB-INF/views/yoilError.jsp
+    		return "yoilError";  // 유효하지 않으면, /WEB-INF/views/yoilError.jsp로 이동
     	
         // 2. 작업
-        Calendar cal = Calendar.getInstance();
-        cal.set(year, month - 1, day);
+    	char yoil = getYoil(year, month, day);
 
-        int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
-        char yoil = " 일월화수목금토".charAt(dayOfWeek);
-
+        // 3. Model에 작업 결과 저장
         model.addAttribute("year", year);
         model.addAttribute("month", month);
         model.addAttribute("day", day);
         model.addAttribute("yoil", yoil);
         
-        return "yoil";  // /WEB-INF/views/yoil.jsp
-    } // main
+        // 4. View이름을 반환
+        return "yoil"; // /WEB-INF/views/yoil.jsp
+    }
     
-    public boolean isValid(int year, int month, int day) {    	
+    private char getYoil(int year, int month, int day) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(year, month - 1, day);
+
+        int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+        return " 일월화수목금토".charAt(dayOfWeek);
+    }
+    
+    private boolean isValid(int year, int month, int day) {    
+    	if(year==-1 || month==-1 || day==-1) 
+    		return false;
+    	
     	return (1<=month && month<=12) && (1<=day && day<=31); // 간단히 체크 
     }
-} // class
+}
