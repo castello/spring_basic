c15240ffe0068f5a2c01e818cbb6cbff8fae97ef
SEONG NAMKUNG <castello@naver.com>
2021-10-03 07:40:21 +0900

    Update YoilTellerMVC4.java

--- ch2/YoilTellerMVC4.java
+++ ch2/YoilTellerMVC4.java
@@ -13,21 +13,22 @@ public class YoilTellerMVC4 {
     public ModelAndView main(MyDate date) { // 반환 타입이 ModelAndView
 System.out.println("date="+date);
 
-    	// 1. ModelAndView를 생성하고, 기본 뷰를 지정
+    	// 1. ModelAndView를 생성
     	ModelAndView mv = new ModelAndView(); 
-    	mv.setViewName("yoilError"); // 뷰의 이름을 지정 
     	
     	// 2. 유효성 검사 
-    	if(!isValid(date.year, date.month, date.day))
+    	if(!isValid(date)) {
+            mv.setViewName("yoilError"); // 뷰의 이름을 지정 
     		return mv;
+        }
     	
         // 3. 작업
-    	char yoil = getYoil(date.year, date.month, date.day);
+    	char yoil = getYoil(date);
 
     	// 4. ModelAndView에 작업한 결과를 저장 
-      	mv.addObject("year",  date.year);     	
-      	mv.addObject("month", date.month);     	
-      	mv.addObject("day",   date.day);     	
+      	mv.addObject("year",  date.getYear());     	
+      	mv.addObject("month", date.getMonth());     	
+      	mv.addObject("day",   date.getDay());     	
       	mv.addObject("yoil", yoil);        
         
       	// 5. 작업 결과를 보여줄 뷰의 이름을 지정 
@@ -36,6 +37,10 @@ System.out.println("date="+date);
       	// 6. ModelAndView를 반환
       	return mv;
     }
+
+    private char getYoil(MyDate date) {
+        return getYoil(date.getYear(), date.getMonth(), date.getDay());
+    }
     
     private char getYoil(int year, int month, int day) {
         Calendar cal = Calendar.getInstance();
@@ -43,6 +48,10 @@ System.out.println("date="+date);
 
         int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
         return " 일월화수목금토".charAt(dayOfWeek);
+    }
+    
+    private boolean isValid(MyDate date) {
+        return isValid(date.getYear(), date.getMonth(), date.getDay());
     }
     
     private boolean isValid(int year, int month, int day) {    
