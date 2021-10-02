8b37664bb98f36031639341bec37b92bfe737469
SEONG NAMKUNG <castello@naver.com>
2021-10-02 23:15:30 +0900

    Update RequestParamTest.java

--- ch2/RequestParamTest.java
+++ ch2/RequestParamTest.java
@@ -13,9 +13,9 @@ public class RequestParamTest {
 	@RequestMapping("/requestParam")
 	public void main(HttpServletRequest request) {
 		String year = request.getParameter("year");
-//		http://localhost:8080/ch2/requestParam         ---->> year=null
-//		http://localhost:8080/ch2/requestParam?year=   ---->> year=""
-//		http://localhost:8080/ch2/requestParam?year    ---->> year=""
+//		http://localhost/ch2/requestParam         ---->> year=null
+//		http://localhost/ch2/requestParam?year=   ---->> year=""
+//		http://localhost/ch2/requestParam?year    ---->> year=""
 		System.out.printf("[%s]year=[%s]%n", new Date(), year);
 	}
 
@@ -23,30 +23,30 @@ public class RequestParamTest {
 //	public void main2(@RequestParam(name="year", required=false) String year) {   // 아래와 동일 
 	public void main2(String year) {   
 //	public void main2(String year) 
-//		http://localhost:8080/ch2/requestParam2         ---->> year=null   404 Not Found
-//		http://localhost:8080/ch2/requestParam2?year    ---->> year=""
+//		http://localhost/ch2/requestParam2         ---->> year=null   404 Not Found
+//		http://localhost/ch2/requestParam2?year    ---->> year=""
 		System.out.printf("[%s]year=[%s]%n", new Date(), year);
 	}
 
 	@RequestMapping("/requestParam3")
 //		public void main3(@RequestParam(name="year", required=true) String year) {   // 아래와 동일 
 		public void main3(@RequestParam String year) {   // 아래와 동일 
-//		http://localhost:8080/ch2/requestParam3         ---->> year=null   400 Bad Request. required=true라서 
-//		http://localhost:8080/ch2/requestParam3?year    ---->> year=""
+//		http://localhost/ch2/requestParam3         ---->> year=null   400 Bad Request. required=true라서 
+//		http://localhost/ch2/requestParam3?year    ---->> year=""
 		System.out.printf("[%s]year=[%s]%n", new Date(), year);
 	}
 
 	@RequestMapping("/requestParam4")
 	public void main4(@RequestParam(required=false) String year) {   
-//		http://localhost:8080/ch2/requestParam4         ---->> year=null 
-//		http://localhost:8080/ch2/requestParam4?year    ---->> year=""   
+//		http://localhost/ch2/requestParam4         ---->> year=null 
+//		http://localhost/ch2/requestParam4?year    ---->> year=""   
 		System.out.printf("[%s]year=[%s]%n", new Date(), year);
 	}
 
 	@RequestMapping("/requestParam5")
 	public void main5(@RequestParam(required=false, defaultValue="1") String year) {   
-//		http://localhost:8080/ch2/requestParam5         ---->> year=1   
-//		http://localhost:8080/ch2/requestParam5?year    ---->> year=1   
+//		http://localhost/ch2/requestParam5         ---->> year=1   
+//		http://localhost/ch2/requestParam5?year    ---->> year=1   
 		System.out.printf("[%s]year=[%s]%n", new Date(), year);
 	}
 	
@@ -54,43 +54,43 @@ public class RequestParamTest {
 	
 	@RequestMapping("/requestParam6") 
 	public void main6(int year) {   
-//		http://localhost:8080/ch2/requestParam6        ---->> 500 java.lang.IllegalStateException: Optional int parameter 'year' is present but cannot be translated into a null value due to being declared as a primitive type. Consider declaring it as object wrapper for the corresponding primitive type.
-//		http://localhost:8080/ch2/requestParam6?year   ---->> 400 Bad Request, nested exception is java.lang.NumberFormatException: For input string: "" 
+//		http://localhost/ch2/requestParam6        ---->> 500 java.lang.IllegalStateException: Optional int parameter 'year' is present but cannot be translated into a null value due to being declared as a primitive type. Consider declaring it as object wrapper for the corresponding primitive type.
+//		http://localhost/ch2/requestParam6?year   ---->> 400 Bad Request, nested exception is java.lang.NumberFormatException: For input string: "" 
 		System.out.printf("[%s]year=[%s]%n", new Date(), year);
 	}
 	
 	@RequestMapping("/requestParam7") 
 	public void main7(@RequestParam int year) {   
-//		http://localhost:8080/ch2/requestParam7        ---->> 400 Bad Request, Required int parameter 'year' is not present
-//		http://localhost:8080/ch2/requestParam7?year   ---->> 400 Bad Request, nested exception is java.lang.NumberFormatException: For input string: "" 
+//		http://localhost/ch2/requestParam7        ---->> 400 Bad Request, Required int parameter 'year' is not present
+//		http://localhost/ch2/requestParam7?year   ---->> 400 Bad Request, nested exception is java.lang.NumberFormatException: For input string: "" 
 		System.out.printf("[%s]year=[%s]%n", new Date(), year);
 	}
 
 	@RequestMapping("/requestParam8") 
 	public void main8(@RequestParam(required=false) int year) {   
-//		http://localhost:8080/ch2/requestParam8        ---->> 500 java.lang.IllegalStateException: Optional int parameter 'year' is present but cannot be translated into a null value due to being declared as a primitive type. Consider declaring it as object wrapper for the corresponding primitive type.
-//		http://localhost:8080/ch2/requestParam8?year   ---->> 400 Bad Request, nested exception is java.lang.NumberFormatException: For input string: "" 
+//		http://localhost/ch2/requestParam8        ---->> 500 java.lang.IllegalStateException: Optional int parameter 'year' is present but cannot be translated into a null value due to being declared as a primitive type. Consider declaring it as object wrapper for the corresponding primitive type.
+//		http://localhost/ch2/requestParam8?year   ---->> 400 Bad Request, nested exception is java.lang.NumberFormatException: For input string: "" 
 		System.out.printf("[%s]year=[%s]%n", new Date(), year);
 	}
 	
 	@RequestMapping("/requestParam9") 
 	public void main9(@RequestParam(required=true) int year) {   
-//		http://localhost:8080/ch2/requestParam9        ---->> 400 Bad Request, Required int parameter 'year' is not present
-//		http://localhost:8080/ch2/requestParam9?year   ---->> 400 Bad Request, nested exception is java.lang.NumberFormatException: For input string: "" 
+//		http://localhost/ch2/requestParam9        ---->> 400 Bad Request, Required int parameter 'year' is not present
+//		http://localhost/ch2/requestParam9?year   ---->> 400 Bad Request, nested exception is java.lang.NumberFormatException: For input string: "" 
 		System.out.printf("[%s]year=[%s]%n", new Date(), year);
 	}
 	
 	@RequestMapping("/requestParam10")   
 	public void main10(@RequestParam(required=true, defaultValue="1") int year) {   
-//		http://localhost:8080/ch2/requestParam10        ---->> year=1   
-//		http://localhost:8080/ch2/requestParam10?year   ---->> year=1   
+//		http://localhost/ch2/requestParam10        ---->> year=1   
+//		http://localhost/ch2/requestParam10?year   ---->> year=1   
 		System.out.printf("[%s]year=[%s]%n", new Date(), year);
 	}
 
 	@RequestMapping("/requestParam11")   
 	public void main11(@RequestParam(required=false, defaultValue="1") int year) {   
-//		http://localhost:8080/ch2/requestParam11        ---->> year=1   
-//		http://localhost:8080/ch2/requestParam11?year   ---->> year=1   
+//		http://localhost/ch2/requestParam11        ---->> year=1   
+//		http://localhost/ch2/requestParam11?year   ---->> year=1   
 		System.out.printf("[%s]year=[%s]%n", new Date(), year);
 	}
 } // class
