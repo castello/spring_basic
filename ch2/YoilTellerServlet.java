fd4b9afe86938a70140e23d801c5a8e820def803
SEONG NAMKUNG <castello@naver.com>
2021-10-14 20:42:24 +0900

    Create YoilTellerServlet.java

---
+++ ch2/YoilTellerServlet.java
@@ -0,0 +1,49 @@
+package com.fastcampus.ch2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Calendar;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/getYoil2") 
+public class YoilTellerServlet extends HttpServlet {
+	@Override
+	public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // 1. 입력
+        String year  = request.getParameter("year");
+        String month = request.getParameter("month");
+        String day   = request.getParameter("day");
+
+        int yyyy = Integer.parseInt(year);
+        int mm   = Integer.parseInt(month);
+        int dd   = Integer.parseInt(day);
+
+        // 2. 처리
+        Calendar cal = Calendar.getInstance();
+        cal.set(yyyy, mm - 1, dd);
+
+        int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+        char yoil = " 일월화수목금토".charAt(dayOfWeek);
+
+        // 3. 출력
+        response.setContentType("text/html");    // 응답의 형식을 html로 지정
+        response.setCharacterEncoding("utf-8");  // 응답의 인코딩을 utf-8로 지정
+
+        PrintWriter out = response.getWriter();  // 브라우저로의 출력 스트림(out)을 얻는다.
+        out.println("<html>");
+        out.println("<head>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println(year + "년 " + month + "월 " + day + "일은 ");
+        out.println(yoil + "요일입니다.");
+        out.println("<body>");
+        out.println("</body>");
+        out.println("</html>");
+        out.close();
+    }
+}
+
