9b09cfad9cfe6e71ecdb645bb553ff0ee984f460
SEONG NAMKUNG <castello@naver.com>
2021-10-26 21:51:08 +0900

    Update TwoDiceServlet.java

--- ch2/TwoDiceServlet.java
+++ ch2/TwoDiceServlet.java
@@ -2,6 +2,7 @@ package com.fastcampus.ch2;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Random;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,10 +11,13 @@ import javax.servlet.http.HttpServletResponse;
 	
 @WebServlet("/rollDice2")
 public class TwoDiceServlet extends HttpServlet {
-    @Override
+	int getRandomInt(int range) {
+		return new Random().nextInt(range)+1;
+	}
+
     public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        int idx1 = (int)(Math.random()*6)+1;
-        int idx2 = (int)(Math.random()*6)+1;
+        int idx1 = getRandomInt(6);
+        int idx2 = getRandomInt(6);
 
         response.setContentType("text/html");
         response.setCharacterEncoding("utf-8");
@@ -26,6 +30,6 @@ public class TwoDiceServlet extends HttpServlet {
         out.println("<img src='resources/img/dice"+idx2+".jpg'>");
         out.println("</body>");
         out.println("</html>");
-        out.close();
+        out.close();	    
     }
 }
