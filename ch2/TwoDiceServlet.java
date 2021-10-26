a61b9f6b94b2fb0d20d2337f4d597327e661c22d
SEONG NAMKUNG <castello@naver.com>
2021-10-26 21:51:39 +0900

    Update TwoDiceServlet.java

--- ch2/TwoDiceServlet.java
+++ ch2/TwoDiceServlet.java
@@ -11,9 +11,9 @@ import javax.servlet.http.HttpServletResponse;
 	
 @WebServlet("/rollDice2")
 public class TwoDiceServlet extends HttpServlet {
-	int getRandomInt(int range) {
-		return new Random().nextInt(range)+1;
-	}
+    int getRandomInt(int range) {
+	return new Random().nextInt(range)+1;
+    }
 
     public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
         int idx1 = getRandomInt(6);
