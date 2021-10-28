5e27ab88329c6a366a45de31642432dda77b1354
SEONG NAMKUNG <castello@naver.com>
2021-10-28 16:18:12 +0900

    Create User.java

---
+++ ch2/User.java
@@ -0,0 +1,47 @@
+package com.fastcampus.ch2;
+
+public class User {
+	private String id;
+	private String pwd;
+	private String name;
+	private String email;
+	private String birth;
+	private String[] sns;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+	public String[] getSns() {
+		return sns;
+	}
+	public void setSns(String[] sns) {
+		this.sns = sns;
+	}
+}
