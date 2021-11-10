f38a085847c7df8c4eb03b263a985e71c06dcddc
SEONG NAMKUNG <castello@naver.com>
2021-11-10 12:04:35 +0900

    Update YoilTellerMVC2.java

--- ch2/YoilTellerMVC2.java
+++ ch2/YoilTellerMVC2.java
@@ -4,18 +4,22 @@ import java.util.Calendar;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class YoilTellerMVC2 { 
-    @RequestMapping("/getYoilMVC2") // http://localhost/ch2/getYoilMVC2 
-    public String main(
-    		@RequestParam(defaultValue="-1") int year, 
-    		@RequestParam(defaultValue="-1") int month, 
-    		@RequestParam(defaultValue="-1") int day, 
-    		Model model
-    ) {
+public class YoilTellerMVC2 {
+	@ExceptionHandler(Exception.class)
+	public String catcher(Exception ex) {
+		return "yoilError";
+	}
+	
+    @RequestMapping("/getYoilMVC2") // http://localhost/ch2/getYoilMVC2
+    public String main(@RequestParam(required=true) int year, 
+    		@RequestParam(required=true) int month, 
+    		@RequestParam(required=true) int day, Model model) {
+ 
         // 1. 유효성 검사
     	if(!isValid(year, month, day)) 
     		return "yoilError";  // 유효하지 않으면, /WEB-INF/views/yoilError.jsp로 이동
@@ -30,8 +34,8 @@ public class YoilTellerMVC2 {
         model.addAttribute("yoil", yoil);
         
         // 4. 작업 결과를 보여줄 View의 이름을 반환
-        return "yoil";  // /WEB-INF/views/yoil.jsp
-    } // main
+        return "yoil"; // /WEB-INF/views/yoil.jsp
+    }
     
     private char getYoil(int year, int month, int day) {
         Calendar cal = Calendar.getInstance();
@@ -47,4 +51,4 @@ public class YoilTellerMVC2 {
     	
     	return (1<=month && month<=12) && (1<=day && day<=31); // 간단히 체크 
     }
-} // class
+} 
