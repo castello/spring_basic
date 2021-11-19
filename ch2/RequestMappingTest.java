7f15379115ef38f02f0d3547640df59be4168541
SEONG NAMKUNG <castello@naver.com>
2021-11-20 06:58:05 +0900

    Update RequestMappingTest.java

--- ch2/RequestMappingTest.java
+++ ch2/RequestMappingTest.java
@@ -5,38 +5,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RequestMappingTest {
-	@RequestMapping("/login/hello.do") // http://localhost/ch2/login/hello.do
+//  @RequestMapping({"/login/hello.do", "/login/hi.do"}) 
+    @RequestMapping("/login/hello.do") // http://localhost/ch2/login/hello.do
     public void test1(){
         System.out.println("urlpattern=/login/hello.do");
     }
 
-	@RequestMapping({"/login/hello.do", "/login/hi.do"}) // http://localhost/ch2/login/hi.do
-    public void test2(){
-        System.out.println("urlpattern={\"/login/hello.do\", \"/login/hi.do\"}");
-    }
-
     @RequestMapping("/login/*")   // /login/hello, /login/hi
-    public void test3(){
+    public void test2(){
         System.out.println("urlpattern=/login/*");
     }
 
     @RequestMapping("/login/**/tmp/*.do")   // /login/hello, /login/hi
-    public void test4(){
+    public void test3(){
         System.out.println("urlpattern=/login/**/tmp/*.do");
     }
 
     @RequestMapping("/login/??")
-    public void test5(){   // /login/hi, /login/my.car
+    public void test4(){   // /login/hi, /login/my.car
         System.out.println("urlpattern=/login/??");
     }
 
     @RequestMapping("*.do") // /hello.do, /hi.do, /login/hi.do
-    public void test6(){ 
+    public void test5(){ 
         System.out.println("urlpattern=*.do");
     }
     
     @RequestMapping("/*.???") //  /hello.aaa, /abc.txt
-    public void test7(){ 
+    public void test6(){ 
         System.out.println("urlpattern=*.???");
     }
 }
