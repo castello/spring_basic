1445502a656b746b2b4ae0e6c89704404d08e441
SEONG NAMKUNG <castello@naver.com>
2022-01-11 15:52:09 +0900

    Create DBConnectionTest2.java

---
+++ ch3/DBConnectionTest2.java
@@ -0,0 +1,54 @@
+package com.fastcampus.ch3;
+
+import org.junit.*;
+import org.springframework.context.*;
+import org.springframework.context.support.*;
+import org.springframework.jdbc.datasource.*;
+
+import javax.sql.*;
+import java.sql.*;
+
+import static org.junit.Assert.*;
+
+public class DBConnectionTest2 {
+    @Test
+    public void jdbcConnectionTest() throws Exception {
+//        // 스키마의 이름(springbasic)이 다른 경우 알맞게 변경해야
+//        String DB_URL = "jdbc:mysql://localhost:3306/springbook?useUnicode=true&characterEncoding=utf8";
+//
+//        // DB의 userid와 pwd를 알맞게 변경해야 함함
+//        String DB_USER = "asdf";
+//        String DB_PASSWORD = "1234";
+//        String DB_DRIVER = "com.mysql.jdbc.Driver";
+//
+//        DriverManagerDataSource ds = new DriverManagerDataSource();
+//        ds.setDriverClassName(DB_DRIVER);
+//        ds.setUrl(DB_URL);
+//        ds.setUsername(DB_USER);
+//        ds.setPassword(DB_PASSWORD);
+
+        ApplicationContext ac = new GenericXmlApplicationContext("file:src/main/webapp/WEB-INF/spring/**/root-context.xml");
+        DataSource ds = ac.getBean(DataSource.class);
+
+        Connection conn = ds.getConnection(); // 데이터베이스의 연결을 얻는다.
+
+        System.out.println("conn = " + conn);
+        assertTrue(conn!=null);
+    }
+}
+
+/* [root-context.xml]
+<?xml version="1.0" encoding="UTF-8"?>
+<beans xmlns="http://www.springframework.org/schema/beans"
+	   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+	   xsi:schemaLocation="http://www.springframework.org/schema/beans https://www.springframework.org/schema/beans/spring-beans.xsd">
+
+	<!-- Root Context: defines shared resources visible to all other web components -->
+	<bean id="dataSource" class="org.springframework.jdbc.datasource.DriverManagerDataSource">
+		<property name="driverClassName" value="com.mysql.jdbc.Driver"></property>
+		<property name="url" value="jdbc:mysql://localhost:3306/springbook?useUnicode=true&amp;characterEncoding=utf8"></property>
+		<property name="username" value="asdf"></property>
+		<property name="password" value="1234"></property>
+	</bean>
+</beans>
+*/
