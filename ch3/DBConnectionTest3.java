cfbd4c15adbd810cda5ccfee08a85f055e4258b3
SEONG NAMKUNG <castello@naver.com>
2022-01-11 15:58:28 +0900

    Create DBConnectionTest3.java

---
+++ ch3/DBConnectionTest3.java
@@ -0,0 +1,34 @@
+package com.fastcampus.ch3;
+
+import org.junit.*;
+import org.junit.runner.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.context.*;
+import org.springframework.context.support.*;
+import org.springframework.jdbc.datasource.*;
+import org.springframework.test.context.*;
+import org.springframework.test.context.junit4.*;
+
+import javax.sql.*;
+import java.sql.*;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
+public class DBConnectionTest3 {
+    @Autowired
+    DataSource ds;
+
+    @Test
+    public void jdbcConnectionTest() throws Exception {
+//        ApplicationContext ac = new GenericXmlApplicationContext("file:src/main/webapp/WEB-INF/spring/**/root-context.xml");
+//        DataSource ds = ac.getBean(DataSource.class);
+
+        Connection conn = ds.getConnection(); // 데이터베이스의 연결을 얻는다.
+
+        System.out.println("conn = " + conn);
+        assertTrue(conn!=null);
+    }
+}
+
