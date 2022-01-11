d4c6414342e93da84402372a0e725c35e4d7dab6
SEONG NAMKUNG <castello@naver.com>
2022-01-11 13:56:47 +0900

    Create DBConnectionTest.java

---
+++ ch3/DBConnectionTest.java
@@ -0,0 +1,26 @@
+package com.fastcampus.ch3;
+
+import java.sql.*;
+
+public class DBConnectionTest {
+    public static void main(String[] args) throws Exception {
+        // 스키마의 이름(springbasic)이 다른 경우 알맞게 변경해야
+        String DB_URL = "jdbc:mysql://localhost:3306/springbasic?useUnicode=true&characterEncoding=utf8";
+
+        // DB의 userid와 pwd를 알맞게 변경해야 함함
+        String DB_USER = "asdf";
+        String DB_PASSWORD = "1234";
+
+        Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD); // 데이터베이스의 연결을 얻는다.
+        Statement stmt = conn.createStatement(); // Statement를 가져온다.
+
+        String query = "SELECT now()"; // 시스템의 현재 날짜시간을 출력하는 쿼리(query)
+        ResultSet rs = stmt.executeQuery(query); // query를 실행한 결과를 rs에 담는다.
+
+        // 실행결과가 담긴 rs에서 한 줄씩 읽어서 출력
+        while (rs.next()) {
+            String curDate = rs.getString(1);
+            System.out.println(curDate);
+        }
+    }
+}
