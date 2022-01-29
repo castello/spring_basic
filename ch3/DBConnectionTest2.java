a8ac10f4d13cee887f92809799dea9d8e4fdab5a
SEONG NAMKUNG <castello@naver.com>
2022-01-29 23:52:53 +0900

    Update DBConnectionTest2.java

--- ch3/DBConnectionTest2.java
+++ ch3/DBConnectionTest2.java
@@ -13,20 +13,6 @@ import static org.junit.Assert.*;
 public class DBConnectionTest2 {
     @Test
     public void jdbcConnectionTest() throws Exception {
-//        // 스키마의 이름(springbasic)이 다른 경우 알맞게 변경해야
-//        String DB_URL = "jdbc:mysql://localhost:3306/springbook?useUnicode=true&characterEncoding=utf8";
-//
-//        // DB의 userid와 pwd를 알맞게 변경해야 함함
-//        String DB_USER = "asdf";
-//        String DB_PASSWORD = "1234";
-//        String DB_DRIVER = "com.mysql.jdbc.Driver";
-//
-//        DataSource ds = new DriverManagerDataSource();
-//        ds.setDriverClassName(DB_DRIVER);
-//        ds.setUrl(DB_URL);
-//        ds.setUsername(DB_USER);
-//        ds.setPassword(DB_PASSWORD);
-
         ApplicationContext ac = new GenericXmlApplicationContext("file:src/main/webapp/WEB-INF/spring/**/root-context.xml");
         DataSource ds = ac.getBean(DataSource.class);
 
