745b837656f1d8eb665f293a48e7b248355def2f
SEONG NAMKUNG <castello@naver.com>
2022-02-03 21:15:23 +0900

    Update RegisterController.java

--- ch3/RegisterController.java
+++ ch3/RegisterController.java
@@ -23,47 +23,28 @@ public class RegisterController {
 	
 	@InitBinder
 	public void toDate(WebDataBinder binder) {
-		ConversionService conversionService = binder.getConversionService();
-//		System.out.println("conversionService="+conversionService);
 //		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 //		binder.registerCustomEditor(Date.class, new CustomDateEditor(df, false));
 		binder.registerCustomEditor(String[].class, "hobby", new StringArrayPropertyEditor("#"));
-//		binder.setValidator(new UserValidator()); // UserValidator를 WebDataBinder의 로컬 validator로 등록 
-//		binder.addValidators(new UserValidator()); // UserValidator를 WebDataBinder의 로컬 validator로 등록 
 		List<Validator> validatorList = binder.getValidators();
 		System.out.println("validatorList="+validatorList);
 	}
 	
-	@RequestMapping(value="/add", method={RequestMethod.GET, RequestMethod.POST})
-//	@GetMapping("/add")
+	@GetMapping("/add")
 	public String register() {
 		return "registerForm"; // WEB-INF/views/registerForm.jsp
 	}
 	
-//	@RequestMapping(value="/save", method=RequestMethod.POST)
 	@PostMapping("/save")  // 4.3부터 
 	public String save(@Valid User user, BindingResult result, Model m) throws Exception {
 		System.out.println("result="+result);
 		System.out.println("user="+user);
-		
-		// 수동 검증 - Validaotor를 직접 생성하고, validate()를 직접 호출 
-//		UserValidator userValidator = new UserValidator();
-//		userValidator.validate(user, result); // BindingResult는 Errors의 자손 
-		
+				
 		// User객체를 검증한 결과 에러가 있으면, registerForm을 이용해서 에러를 보여줘야 함.
 		if(result.hasErrors()) {
 			return "registerForm"; 
 		}
-		
-//		// 1. 유효성 검사
-//		if(!isValid(user)) {
-//			String msg = URLEncoder.encode("id를 잘못입력하셨습니다.", "utf-8");
-//			
-//			m.addAttribute("msg", msg);
-//			return "forward:/register/add";
-////			return "redirect:/register/add?msg="+msg; // URL재작성(rewriting)
-//		}
-		
+				
 		// 2. DB에 신규회원 정보를 저장 
 		return "registerInfo";
 	}
