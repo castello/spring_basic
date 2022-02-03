94f5cfbf1ca89645d6df917eec1f8837107bd9e6
SEONG NAMKUNG <castello@naver.com>
2022-02-03 20:49:29 +0900

    Create GlobalValidator.java

---
+++ ch2/GlobalValidator.java
@@ -0,0 +1,32 @@
+package com.fastcampus.ch2;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+public class GlobalValidator implements Validator {
+	@Override
+	public boolean supports(Class<?> clazz) {
+//		return User.class.equals(clazz); // 검증하려는 객체가 User타입인지 확인
+		return User.class.isAssignableFrom(clazz); // clazz가 User 또는 그 자손인지 확인
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) { 
+		System.out.println("GlobalValidator.validate() is called");
+
+		User user = (User)target;
+		
+		String id = user.getId();
+		
+//		if(id==null || "".equals(id.trim())) {
+//			errors.rejectValue("id", "required");
+//		}
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "id",  "required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "pwd", "required");
+		
+		if(id==null || id.length() <  5 || id.length() > 12) {
+			errors.rejectValue("id", "invalidLength", new String[]{"5", "12"}, null);
+		}
+	}
+}
