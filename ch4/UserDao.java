c600ddc4ce21cc75b4aeeec274e9428d827bddf0
SEONG NAMKUNG <castello@naver.com>
2022-02-21 13:33:07 +0900

    Update UserDao.java

--- ch4/UserDao.java
+++ ch4/UserDao.java
@@ -3,15 +3,10 @@ package com.fastcampus.ch4.dao;
 import com.fastcampus.ch4.domain.*;
 
 public interface UserDao {
-    int deleteUser(String id);
-
-    User selectUser(String id);
-
-    // 사용자 정보를 user_info테이블에 저장하는 메서드
-    int insertUser(User user);
-
-    // 매개변수로 받은 사용자 정보로 user_info테이블을 update하는 메서드
-    int updateUser(User user);
-
+    User selectUser(String id) throws Exception;
+    int deleteUser(String id) throws Exception;
+    int insertUser(User user) throws Exception;
+    int updateUser(User user) throws Exception;
+    int count() throws Exception;
     void deleteAll() throws Exception;
 }
