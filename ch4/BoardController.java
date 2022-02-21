1b8e838f6dbe435f5c1de6e12d5214395e7fbfde
SEONG NAMKUNG <castello@naver.com>
2022-02-21 17:32:01 +0900

    Update BoardController.java

--- ch4/BoardController.java
+++ ch4/BoardController.java
@@ -1,20 +1,28 @@
-package com.fastcampus.ch4;
+package com.fastcampus.ch4.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+import com.fastcampus.ch4.domain.*;
+import com.fastcampus.ch4.service.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.*;
+import org.springframework.ui.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.*;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import javax.servlet.http.*;
+import java.time.*;
+import java.util.*;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
+	@Autowired
+	BoardService boardService;
+
 	@GetMapping("/list")
 	public String list(HttpServletRequest request) {
 		if(!loginCheck(request))
 			return "redirect:/login/login?toURL="+request.getRequestURL();  // 로그인을 안했으면 로그인 화면으로 이동
-		
+
 		return "boardList"; // 로그인을 한 상태이면, 게시판 화면으로 이동
 	}
 
