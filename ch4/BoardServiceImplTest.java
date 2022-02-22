65525b99ac0f405fd783accbf84b33c28ac11a40
SEONG NAMKUNG <castello@naver.com>
2022-02-22 13:22:01 +0900

    Create BoardServiceImplTest.java

---
+++ ch4/BoardServiceImplTest.java
@@ -0,0 +1,34 @@
+package com.fastcampus.ch4.service;
+
+import org.junit.*;
+
+import java.time.*;
+
+import static org.junit.Assert.*;
+
+public class BoardServiceImplTest {
+
+    @Test
+    public void read() {
+    }
+
+    @Test
+    public void edit() {
+    }
+
+    @Test
+    public void write() {
+    }
+
+    @Test
+    public void remove() {
+    }
+
+    @Test
+    public void getPage() {
+    }
+
+    @Test
+    public void getList() {
+    }
+}
