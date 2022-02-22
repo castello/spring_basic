011002afbd8e9f293d9bd95898db4c4e8c36eed5
SEONG NAMKUNG <castello@naver.com>
2022-02-22 15:06:25 +0900

    Create BoardDao.java

---
+++ ch4/BoardDao.java
@@ -0,0 +1,17 @@
+package com.fastcampus.ch4.dao;
+
+import com.fastcampus.ch4.domain.*;
+
+import java.util.*;
+
+public interface BoardDao {
+    BoardDto select(Integer bno) throws Exception;
+    int delete(Integer bno) throws Exception;
+    int insert(BoardDto dto) throws Exception;
+    int update(BoardDto dto) throws Exception;
+
+    List<BoardDto> selectPage(Map map) throws Exception;
+    List<BoardDto> selectAll() throws Exception;
+    int deleteAll() throws Exception;
+    int count() throws Exception;
+}
