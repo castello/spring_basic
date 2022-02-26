758a2a8e5aa2b9da943e577be110a5e266e7ea67
SEONG NAMKUNG <castello@naver.com>
2022-02-26 15:14:45 +0900

    Update BoardDao.java

--- ch4/BoardDao.java
+++ ch4/BoardDao.java
@@ -6,11 +6,11 @@ import java.util.*;
 
 public interface BoardDao {
     BoardDto select(Integer bno) throws Exception;
-    int delete(Integer bno) throws Exception;
+    int delete(Integer bno, String writer) throws Exception;
     int insert(BoardDto dto) throws Exception;
     int update(BoardDto dto) throws Exception;
     int increaseViewCnt(Integer bno) throws Exception;
-    
+
     List<BoardDto> selectPage(Map map) throws Exception;
     List<BoardDto> selectAll() throws Exception;
     int deleteAll() throws Exception;
