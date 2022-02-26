d91995c26e1dd1207795e33ae3dbc3ddea359f35
SEONG NAMKUNG <castello@naver.com>
2022-02-26 15:12:11 +0900

    Create BoardDaoImpl.java

---
+++ ch4/BoardDaoImpl.java
@@ -0,0 +1,63 @@
+package com.fastcampus.ch4.dao;
+
+import com.fastcampus.ch4.dao.*;
+import com.fastcampus.ch4.domain.*;
+import org.apache.ibatis.session.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.*;
+
+import java.util.*;
+
+@Repository
+public class BoardDaoImpl implements BoardDao {
+    @Autowired
+    private SqlSession session;
+    private static String namespace = "com.fastcampus.ch4.dao.BoardMapper.";
+
+    public int count() throws Exception {
+        return session.selectOne(namespace+"count");
+    } // T selectOne(String statement)
+
+    @Override
+    public int deleteAll() {
+        return session.delete(namespace+"deleteAll");
+    }
+
+    @Override
+    public int delete(Integer bno, String writer) throws Exception {
+        Map map = new HashMap();
+        map.put("bno", bno);
+        map.put("writer", writer);
+        return session.delete(namespace+"delete", map);
+    } // int delete(String statement, Object parameter)
+
+    public int insert(BoardDto dto) throws Exception {
+        return session.insert(namespace+"insert", dto);
+    } // int insert(String statement, Object parameter)
+
+    @Override
+    public List<BoardDto> selectAll() throws Exception {
+        return session.selectList(namespace+"selectAll");
+    } // List<E> selectList(String statement)
+
+    public BoardDto select(Integer bno) throws Exception {
+        return session.selectOne(namespace + "select", bno);
+    } // T selectOne(String statement, Object parameter)
+
+    @Override
+    public List<BoardDto> selectPage(Map map) throws Exception {
+        return session.selectList(namespace+"selectPage", map);
+    } // List<E> selectList(String statement, Object parameter)
+
+    @Override
+    public int update(BoardDto dto) throws Exception {
+        return session.update(namespace+"update", dto);
+    } // int update(String statement, Object parameter)
+
+    @Override
+    public int increaseViewCnt(Integer bno) throws Exception {
+        return session.update(namespace+"increaseViewCnt", bno);
+    } // int update(String statement, Object parameter)
+
+
+}
