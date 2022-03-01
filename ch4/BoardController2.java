4b55d193488d67c7db9602da20716f5abe495c0c
SEONG NAMKUNG <castello@naver.com>
2022-03-01 13:12:54 +0900

    Create BoardController2.java

---
+++ ch4/BoardController2.java
@@ -0,0 +1,35 @@
+package com.fastcampus.ch4.controller;
+
+import com.fastcampus.ch4.domain.*;
+import com.fastcampus.ch4.service.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.*;
+import org.springframework.ui.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.*;
+
+import javax.servlet.http.*;
+import java.time.*;
+import java.util.*;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+	@Autowired
+	BoardService boardService;
+
+	@GetMapping("/list")
+	public String list(HttpServletRequest request) {
+		if(!loginCheck(request))
+			return "redirect:/login/login?toURL="+request.getRequestURL();  // 로그인을 안했으면 로그인 화면으로 이동
+
+		return "boardList"; // 로그인을 한 상태이면, 게시판 화면으로 이동
+	}
+
+	private boolean loginCheck(HttpServletRequest request) {
+		// 1. 세션을 얻어서
+		HttpSession session = request.getSession();
+		// 2. 세션에 id가 있는지 확인, 있으면 true를 반환
+		return session.getAttribute("id")!=null;
+	}
+}
