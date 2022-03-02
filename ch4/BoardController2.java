4e3ef554a97120151ec6ce1ed7a02801f91c7fbc
SEONG NAMKUNG <castello@naver.com>
2022-03-02 16:45:44 +0900

    Update BoardController2.java

--- ch4/BoardController2.java
+++ ch4/BoardController2.java
@@ -15,21 +15,136 @@ import java.util.*;
 @Controller
 @RequestMapping("/board")
 public class BoardController {
-	@Autowired
-	BoardService boardService;
+    @Autowired
+    BoardService boardService;
 
-	@GetMapping("/list")
-	public String list(HttpServletRequest request) {
-		if(!loginCheck(request))
-			return "redirect:/login/login?toURL="+request.getRequestURL();  // 로그인을 안했으면 로그인 화면으로 이동
+    @PostMapping("/modify")
+    public String modify(BoardDto boardDto, Integer page, Integer pageSize, RedirectAttributes rattr, Model m, HttpSession session) {
 
-		return "boardList"; // 로그인을 한 상태이면, 게시판 화면으로 이동
-	}
+        String writer = (String)session.getAttribute("id");
+        boardDto.setWriter(writer);
 
-	private boolean loginCheck(HttpServletRequest request) {
-		// 1. 세션을 얻어서
-		HttpSession session = request.getSession();
-		// 2. 세션에 id가 있는지 확인, 있으면 true를 반환
-		return session.getAttribute("id")!=null;
-	}
+        try {
+            if (boardService.modify(boardDto)!= 1)
+                throw new Exception("Modify failed.");
+
+            rattr.addAttribute("page", page);
+            rattr.addAttribute("pageSize", pageSize);
+            rattr.addFlashAttribute("msg", "MOD_OK");
+            return "redirect:/board/list";
+        } catch (Exception e) {
+            e.printStackTrace();
+            m.addAttribute(boardDto);
+            m.addAttribute("page", page);
+            m.addAttribute("pageSize", pageSize);
+            m.addAttribute("msg", "MOD_ERR");
+            return "board"; // 등록하려던 내용을 보여줘야 함.
+        }
+    }
+
+    @GetMapping("/write")
+    public String write(Model m) {
+        m.addAttribute("mode", "new");
+
+        return "board";
+    }
+
+    @PostMapping("/write") // insert니까 delete인 remove하고 동일
+    public String write(BoardDto boardDto, RedirectAttributes rattr, HttpSession session) {
+        String writer = (String)session.getAttribute("id");
+        boardDto.setWriter(writer);
+
+        try {
+            if (boardService.write(boardDto) != 1)
+                throw new Exception("Write failed.");
+
+            rattr.addFlashAttribute("msg", "WRT_OK");
+            return "redirect:/board/list";
+        } catch (Exception e) {
+            e.printStackTrace();
+            rattr.addAttribute(boardDto);
+            rattr.addFlashAttribute("msg", "WRT_ERR");
+            return "board"; // 등록하려던 내용을 보여줘야 함.
+        }
+    }
+
+    @GetMapping("/read")
+    public String read(Integer bno, Integer page, Integer pageSize, RedirectAttributes rattr, Model m) {
+        try {
+            BoardDto boardDto = boardService.read(bno);
+            m.addAttribute(boardDto);
+            m.addAttribute("page", page);
+            m.addAttribute("pageSize", pageSize);
+        } catch (Exception e) {
+            e.printStackTrace();
+            rattr.addAttribute("page", page);
+            rattr.addAttribute("pageSize", pageSize);
+            rattr.addFlashAttribute("msg", "READ_ERR");
+            return "redirect:/board/list";
+        }
+
+        return "board";
+    }
+
+    @PostMapping("/remove")
+    public String remove(Integer bno, Integer page, Integer pageSize, RedirectAttributes rattr, HttpSession session) {
+        String writer = (String)session.getAttribute("id");
+        String msg = "DEL_OK";
+
+        try {
+            if(boardService.remove(bno, writer)!=1)
+                throw new Exception("Delete failed.");
+        } catch (Exception e) {
+            e.printStackTrace();
+            msg = "DEL_ERR";
+        }
+
+        rattr.addAttribute("page", page);
+        rattr.addAttribute("pageSize", pageSize);
+        rattr.addFlashAttribute("msg", msg);
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/list")
+    public String list(@RequestParam(defaultValue ="1") Integer page,
+                       @RequestParam(defaultValue = "10") Integer pageSize,Model m, HttpServletRequest request) {
+        if(!loginCheck(request))
+            return "redirect:/login/login?toURL="+request.getRequestURL(); // 로그인을 안했으면 로그인 화면으로 이동
+
+        try {
+            int totalCnt = boardService.getCount();
+            m.addAttribute("totalCnt", totalCnt);
+
+            PageHandler pageHandler = new PageHandler(totalCnt, page, pageSize);
+
+            if(page < 0 || page > pageHandler.getTotalPage())
+                page = 1;
+            if(pageSize < 0 || pageSize > 50)
+                pageSize = 10;
+
+            Map map = new HashMap();
+            map.put("offset", (page-1)*pageSize);
+            map.put("pageSize", pageSize);
+
+            List<BoardDto> list = boardService.getPage(map);
+            m.addAttribute("list", list);
+            m.addAttribute("ph", pageHandler);
+
+            Instant startOfToday = LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant();
+            m.addAttribute("startOfToday", startOfToday.toEpochMilli());
+        } catch (Exception e) {
+            e.printStackTrace();
+            m.addAttribute("msg", "LIST_ERR");
+            m.addAttribute("totalCnt", 0);
+        }
+
+        return "boardList"; // 로그인을 한 상태이면, 게시판 화면으로 이동
+    }
+
+    private boolean loginCheck(HttpServletRequest request) {
+        // 1. 세션을 얻어서(false는 session이 없어도 새로 생성하지 않는다. 반환값 null)
+        HttpSession session = request.getSession(false);
+        // 2. 세션에 id가 있는지 확인, 있으면 true를 반환
+        return session!=null && session.getAttribute("id")!=null;
+    }
 }
