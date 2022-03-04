c5d0cdf1df947c5e37e49eb2fb4a85b1aa88f941
SEONG NAMKUNG <castello@naver.com>
2022-03-04 18:26:37 +0900

    Create PageHandler.java

---
+++ ch4/PageHandler.java
@@ -0,0 +1,127 @@
+package com.fastcampus.ch4.domain;
+
+import org.springframework.web.util.*;
+
+public class PageHandler {
+    private SearchCondition sc;
+//    private int pageSize = 10; // 한 페이지당 게시물 갯수
+//    private int page; // 현재 페이지
+//    private String  option;
+//    private String  keyword;
+    public  final int NAV_SIZE = 10; // page navigation size
+    private int totalCnt; // 게시물의 총 갯수
+    private int totalPage; // 전체 페이지의 갯수
+    private int beginPage; // 화면에 보여줄 첫 페이지
+    private int endPage; // 화면에 보여줄 마지막 페이지
+    private boolean showNext = false; // 이후를 보여줄지의 여부. endPage==totalPage이면, showNext는 false
+    private boolean showPrev = false; // 이전을 보여줄지의 여부. beginPage==1이 아니면 showPrev는 false
+
+    public PageHandler(int totalCnt, Integer page) {
+        this(totalCnt, new SearchCondition(page, 10));
+    }
+
+    public PageHandler(int totalCnt, Integer page, Integer pageSize) {
+        this(totalCnt, new SearchCondition(page, pageSize));
+    }
+
+    public PageHandler(int totalCnt, SearchCondition sc) {
+        this.totalCnt = totalCnt;
+        this.sc = sc;
+
+        doPaging(totalCnt, sc);
+    }
+
+    private void doPaging(int totalCnt, SearchCondition sc) {
+        this.totalPage = totalCnt / sc.getPageSize() + (totalCnt % sc.getPageSize()==0? 0:1);
+        this.sc.setPage(Math.min(sc.getPage(), totalPage));  // page가 totalPage보다 크지 않게
+        this.beginPage = (this.sc.getPage() -1) / NAV_SIZE * NAV_SIZE + 1; // 11 -> 11, 10 -> 1, 15->11. 따로 떼어내서 테스트
+        this.endPage = Math.min(beginPage + NAV_SIZE - 1, totalPage);
+        this.showPrev = beginPage!=1;
+        this.showNext = endPage!=totalPage;
+    }
+
+    void print() { // String.join()을 써서 변경?
+        System.out.println("curPage="+ sc.getPage());
+        System.out.print(showPrev? "PREV " : "");
+
+        for(int i=beginPage;i<=endPage;i++) {
+            System.out.print(i+" ");
+        }
+        System.out.println(showNext? " NEXT" : "");
+    }
+
+    public SearchCondition getSc() {
+        return sc;
+    }
+
+    public void setSc(SearchCondition sc) {
+        this.sc = sc;
+    }
+
+    public int getTotalCnt() {
+        return totalCnt;
+    }
+
+    public void setTotalCnt(int totalCnt) {
+        this.totalCnt = totalCnt;
+    }
+
+    public boolean isShowNext() {
+        return showNext;
+    }
+
+    public void setShowNext(boolean showNext) {
+        this.showNext = showNext;
+    }
+
+    public int getBeginPage() {
+        return beginPage;
+    }
+
+    public void setBeginPage(int beginPage) {
+        this.beginPage = beginPage;
+    }
+
+    public int getNAV_SIZE() {
+        return NAV_SIZE;
+    }
+
+    public int getTotalPage() {
+        return totalPage;
+    }
+
+    public void setTotalPage(int totalPage) {
+        this.totalPage = totalPage;
+    }
+
+    public int getEndPage() {
+        return endPage;
+    }
+
+    public void setEndPage(int endPage) {
+        this.endPage = endPage;
+    }
+
+    public boolean isShowPrev() {
+        return showPrev;
+    }
+
+    public void setShowPrev(boolean showPrev) {
+        this.showPrev = showPrev;
+    }
+
+    @Override
+    public String toString() {
+        return "PageHandler{" +
+                "sc=" + sc +
+                ", totalCnt=" + totalCnt +
+                ", showNext=" + showNext +
+                ", beginPage=" + beginPage +
+                ", NAV_SIZE=" + NAV_SIZE +
+                ", totalPage=" + totalPage +
+                ", endPage=" + endPage +
+                ", showPrev=" + showPrev +
+                '}';
+    }
+}
+
