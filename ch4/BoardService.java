b339f4b7c2ce6c09fa41a0d517e059b5240b7b39
SEONG NAMKUNG <castello@naver.com>
2022-03-04 18:29:47 +0900

    Update BoardService.java

--- ch4/BoardService.java
+++ ch4/BoardService.java
@@ -6,16 +6,13 @@ import java.util.*;
 
 public interface BoardService {
     int getCount() throws Exception;
-
     int remove(Integer bno, String writer) throws Exception;
-
     int write(BoardDto boardDto) throws Exception;
-
     List<BoardDto> getList() throws Exception;
-
     BoardDto read(Integer bno) throws Exception;
-
     List<BoardDto> getPage(Map map) throws Exception;
-
     int modify(BoardDto boardDto) throws Exception;
+
+    int getSearchResultCnt(SearchCondition sc) throws Exception;
+    List<BoardDto> getSearchResultPage(SearchCondition sc) throws Exception;
 }
