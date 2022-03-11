d8a2f0571111b84e36a79249aa09fd4c667d21ed
SEONG NAMKUNG <castello@naver.com>
2022-03-12 05:25:06 +0900

    Update BoardServiceImpl.java

--- ch4/BoardServiceImpl.java
+++ ch4/BoardServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.*;
 
 import java.util.*;
 
-@Repository
+@Service
 public class BoardServiceImpl implements BoardService {
     @Autowired
     BoardDao boardDao;
